8b0f2f22cd97363423a63709b666a90471155547
GDChernobog <vdjurovic@griddynamics.com>
2024-01-03 21:17:35 +0100

    Dodali 2 TF folder-a za PR i NP

--- Terraform/NP/main.tf
+++ Terraform/NP/main.tf
@@ -5,22 +5,22 @@ provider "google" {
 }
 
 # Create a GKE cluster with zero initial nodes
-resource "google_container_cluster" "vdjurovic_cluster_np" {
-  name     = "vdjurovic-cluster-np"
-  location = "us-central1"
-  deletion_protection = false
-  initial_node_count = 1
-  #remove_default_node_pool = true
-}
-resource "google_storage_bucket" "terraform_state_np" {
-  name          = "viv_bucket_np"
-  location      = "US"
-  force_destroy = true
-
-  versioning {
-    enabled = true
-  }
-}
+#resource "google_container_cluster" "vdjurovic_cluster_np" {
+#  name     = "vdjurovic-cluster-np"
+#  location = "us-central1"
+#  deletion_protection = false
+#  initial_node_count = 1
+#  #remove_default_node_pool = true
+#}
+#resource "google_storage_bucket" "terraform_state_np" {
+#  name          = "viv_bucket_np"
+#  location      = "US"
+#  force_destroy = true
+#
+#  versioning {
+#    enabled = true
+#  }
+#}
 
 
 
