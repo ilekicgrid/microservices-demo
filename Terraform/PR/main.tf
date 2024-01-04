edde46cd108bb546808652dc4e9499491f6f1db5
GDChernobog <vdjurovic@griddynamics.com>
2024-01-04 14:59:47 +0100

    Dodali PR i NP helm akciju

--- Terraform/PR/main.tf
+++ Terraform/PR/main.tf
@@ -10,17 +10,29 @@ resource "google_container_cluster" "vdjurovic_cluster_pr" {
   location = "us-central1"
   deletion_protection = false
   initial_node_count = 1
-  #remove_default_node_pool = true
+  remove_default_node_pool = true
 }
-#resource "google_storage_bucket" "terraform_state_pr" {
-#  name          = "viv_bucket_pr"
-#  location      = "US"
-#  force_destroy = true
-#
-#  versioning {
-#    enabled = true
-#  }
-#}
+# Create a node pool with horizontal autoscaling
+resource "google_container_node_pool" "vdjurovic_autoscaling_node_pool" {
+  name       = "vdjurovic-autoscaling-node-pool"
+  location   = "us-central1"
+  cluster    = google_container_cluster.vdjurovic_cluster_pr.name
+  node_count = 1
+
+  autoscaling {
+    min_node_count = 1
+    max_node_count = 5
+  }
+
+  node_config {
+    preemptible  = false
+    machine_type = "e2-medium"
+    disk_size_gb = 150
+    image_type   = "UBUNTU"
+
+    # Additional configurations can be added here
+  }
+}
 
 
 
