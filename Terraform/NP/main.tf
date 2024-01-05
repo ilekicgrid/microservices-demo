36d7995215a8e85d818c0d1bf5b2ca9255d750f2
ilekicgrid <ilekic@griddynamics.com>
2024-01-05 13:36:45 +0100

    test3

--- Terraform/NP/main.tf
+++ Terraform/NP/main.tf
@@ -13,4 +13,22 @@ resource "google_container_cluster" "ilekic_cluster_np" {
   #remove_default_node_pool = true
   enable_autopilot = true
 }
+# Create a node pool with horizontal autoscaling
+resource "google_container_node_pool" "ilekic_autoscaling_node_pool" {
+  name       = "ilekic-autoscaling-node-pool"
+  location   = "us-central1"
+  cluster    = google_container_cluster.ilekic_cluster_np.name
+  node_count = 1
+
+   autoscaling {
+     min_node_count = 1
+     max_node_count = 5
+   }
+
+  node_config {
+    # preemptible  = false
+    machine_type = "e2-medium"
+    disk_size_gb = 150
+  }
+}
 
